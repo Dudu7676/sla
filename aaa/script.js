d7eb8e6fae1fb6e26fa61939bd266b3e7c1cea30
Dudu7676 <165792085+Dudu7676@users.noreply.github.com>
2024-08-14 03:10:23 -0300

    Add files via upload

---
+++ aaa/script.js
@@ -0,0 +1,15 @@
+alert("Boas vindas ao nosso site!");
+//let nome = "Lua";
+//let idade = 25;
+//let numeroDeVendas = 50;
+//let saldoDisponivel = 1000;
+//alert("Erro! Preencha todos os campos");
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro);
+let nome = prompt("digite seu nome?");
+let idade = prompt("digite sua idade?");
+let numeroDeVendas = prompt("digite o número de vendas");
+let saldoDisponivel = prompt("digite o saldo disponivel");
+if (idade >= 18){
+    alert("Pode tirar a habilitação!")
+}
