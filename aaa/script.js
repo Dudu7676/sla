61cd9ba866869124d04807a4d6fc2c90f2d739e2
Dudu7676 <165792085+Dudu7676@users.noreply.github.com>
2024-08-14 03:11:58 -0300

    Update script.js

--- aaa/script.js
+++ aaa/script.js
@@ -1,10 +1,4 @@
 alert("Boas vindas ao nosso site!");
-//let nome = "Lua";
-//let idade = 25;
-//let numeroDeVendas = 50;
-//let saldoDisponivel = 1000;
-//alert("Erro! Preencha todos os campos");
-let mensagemDeErro = "Erro! Preencha todos os campos";
 alert(mensagemDeErro);
 let nome = prompt("digite seu nome?");
 let idade = prompt("digite sua idade?");
