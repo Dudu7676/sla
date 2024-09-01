eecd3f4cc1da4a3fe5e3dcc7fba712d6e7d33124
Dudu7676 <165792085+Dudu7676@users.noreply.github.com>
2024-08-31 22:46:47 -0300

    Add files via upload

---
+++ saldo da conta/script.js
@@ -0,0 +1,10 @@
+let numero = prompt("Digite o numero do saldo");
+
+
+
+if (numero > 0) {
+    alert(`Número do saldo é ${numero} por tanto é positivo`);
+}
+else if (numero < 0) {
+    alert(`Número do saldo é ${numero} por tanto é negativo.`);
+}
